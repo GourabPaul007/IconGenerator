093dbea76123ffe54bd60c5d1d3efdd0fd1f797b
Gourab Paul <gourabpaul900@gmail.com>
2022-04-17 12:09:49 +0530

    made easier to make icons

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
-const icongen = require('icon-gen')
+const fs = require("fs");
 
-icongen('./images', './icons', { report: true })
-  .then((results) => {
-    console.log(results)
-  })
-  .catch((err) => {
-    console.error(err)
-  })
+// const __dirname = "C:\\Projects\\IconMaker\\";
+let imagesPrename = "kalm";
+const directory = __dirname + "\\icons\\icons\\png\\";
+
+// List all the filenames before renaming
+getCurrentFilenames();
+function getCurrentFilenames() {
+  console.log("Current filenames:");
+  fs.readdirSync(directory).forEach((oldFileName) => {
+    let newFileName;
+    if (oldFileName.includes("kalm")) {
+      newFileName = oldFileName;
+    } else {
+      newFileName =
+        imagesPrename +
+        oldFileName.substring(0, oldFileName.indexOf("x")) +
+        ".png";
+    }
+
+    fs.renameSync(directory + oldFileName, directory + newFileName, (err) => {
+      if (err) throw err;
+      else {
+        console.log(
+          `File Renamed: ------------ ${oldFileName} ------------> ${newFileName}`
+        );
+      }
+    });
+  });
+}
--- readme.md
+++ readme.md
@@ -1,1 +1,6 @@
 icon generator using electron-icon-maker & terminal
+
+Step 0 -> npm install
+Step 1 -> Put the image which you want to make into icons in originalImage
+Step 2 -> In console type "npm run makeIcon"
+Step 3 -> Icons Should be ready in `icons` folder
